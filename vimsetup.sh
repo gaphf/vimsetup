b8bc56812f13e269baa7eb537a2e6ec77fc796b4
gaphf <gaborkollar@outlook.com>
2024-02-25 14:50:03 +0100

    Create vimsetup.sh

---
+++ vimsetup.sh
@@ -0,0 +1,8 @@
+curl -fLo ~/.vimrc \
+https://raw.githubusercontent.com/gaphf/vimsetup/main/.vimrc
+
+curl -fLo ~/.vim/autoload/sonokai.vim --create-dirs \
+https://raw.githubusercontent.com/sainnhe/sonokai/master/autoload/sonokai.vim
+
+curl -fLo ~/.vim/colors/sonokai.vim --create-dirs \
+https://raw.githubusercontent.com/sainnhe/sonokai/master/colors/sonokai.vim
